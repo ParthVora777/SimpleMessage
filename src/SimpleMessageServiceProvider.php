f6a8be86a46973c76da55066a5a18f36481a4d4f
Parth Vora <parth.vora.777@gmail.com>
2020-06-06 23:29:37 +0530

    Initial commit

---
+++ src/SimpleMessageServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ParthVora777\SimpleMessage;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleMessageServiceProvider extends ServiceProvider
+{
+    const PACKAGE_NAME = 'parthvora777/simplemessage';
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $installedJsonFile = './../composer/installed.json';
+        if (file_exists($installedJsonFile)) {
+            $installedPackages = json_decode(file_get_contents($installedJsonFile));
+            foreach ($installedPackages as $installedPackage) {
+                if (isset($installedPackage->name) && $installedPackage->name === self::PACKAGE_NAME) {
+                    echo 'A simple message from ' . self::PACKAGE_NAME . ' ' . $installedPackage->version;
+                }
+            }
+        }
+    }
+}
